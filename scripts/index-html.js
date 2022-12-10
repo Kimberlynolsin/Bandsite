a14c0b8c253d5cc6105ba75708e715a3a9e69a75
Kimberly Nolsin <kimberly.nolsin@gmail.com>
2022-12-10 13:44:29 -0500

    Added event listener to submit button

--- scripts/index-html.js
+++ scripts/index-html.js
@@ -1,21 +1,9 @@
-/*
-
-<section class="comments">
-<h2 class="comments__title">Join the Conversation</h2>
-<div class="comments__container">
-
-
-</div>
-</section>
-
-*/
-
 const commentsArray = [
   {
-    user: "Connor Walton",
-    date: "02/17/2021",
+    user: "Miles Acosta",
+    date: "12/20/2020",
     comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+      "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
   {
     user: "Emilie Beach",
@@ -24,14 +12,12 @@ const commentsArray = [
       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
-    user: "Miles Acosta",
-    date: "12/20/2020",
+    user: "Connor Walton",
+    date: "02/17/2021",
     comment:
-      "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
 ];
-
-
 
 const comments = document.querySelector(".comment");
 
@@ -39,120 +25,67 @@ const commentTitle = document.createElement("h2");
 commentTitle.classList.add("comment__title");
 commentTitle.innerText = "Join the Conversation";
 
-const inputField = document.querySelector(".comment__form")
+const inputField = document.querySelector(".comment__form");
 
-comments.appendChild(commentTitle);
-comments.appendChild(inputField)
+comments.prepend(inputField);
+comments.prepend(commentTitle);
 
-/*
-//////////////////////////////////////
- what do we need in the function?
-1. We need to extract comment from commentsArray 
- - (commentsArray[i])
+const commentSection = document.querySelector(".commentSection");
 
-//////////////////////////////////////
+const displayComment = (text) => {
+  const commentContainer = document.createElement("div");
+  commentContainer.classList.add("comment-container");
 
-2. we need to stick it into section
-a) create a div
-- const div = document.createElement("div") 
-- div.classList.add("div__comment")
+  const commentPlaceholder = document.createElement("div");
+  commentPlaceholder.classList.add("comment__placeholder");
 
-b)stick it into the existing div
-- comments.appendChild(div)
+  const commentTimestamp = document.createElement("div");
+  commentTimestamp.classList.add("comment__timestamp");
 
+  const commentName = document.createElement("p");
+  commentName.classList.add("comment__name");
+  commentName.innerText = text.user;
 
-*/
+  const commentDate = document.createElement("p");
+  commentDate.classList.add("comment__date");
+  commentDate.innerText = text.date;
 
+  const pDiv = document.createElement("div");
+  pDiv.classList.add("comment__para");
 
+  const comment = document.createElement("p");
+  comment.classList.add("p");
+  comment.innerText = text.comment;
 
-const displayComment = (text) =>{
-    //create div and add class to it 
-    const commentContainer = document.createElement("div")
-    commentContainer.classList.add("comment-container")
-    
-    const commentPlaceholder = document.createElement("div")
-    commentPlaceholder.classList.add("comment__placeholder")
+  pDiv.appendChild(comment);
+  commentName.appendChild(pDiv);
+  commentTimestamp.appendChild(commentName);
+  commentTimestamp.appendChild(commentDate);
+  commentContainer.appendChild(commentPlaceholder);
+  commentContainer.appendChild(commentTimestamp);
 
-    const commentTimestamp = document.createElement("div")
-    commentTimestamp.classList.add("comment__timestamp")
-    
-    const commentName = document.createElement("p")
-    commentName.classList.add("comment__name")
-    commentName.innerText = text.user
+  commentSection.prepend(commentContainer);
+};
 
-    
-    const commentDate = document.createElement("p")
-    commentDate.classList.add("comment__date")
-    commentDate.innerText = text.date
-
-    const pDiv = document.createElement("div")
-    pDiv.classList.add("comment__para")
-
-    const comment = document.createElement("p")
-    comment.classList.add("p")
-    comment.innerText = text.comment
-
-    commentContainer.appendChild(commentPlaceholder)
-    comments.appendChild(commentContainer)
-
-    commentContainer.appendChild(commentTimestamp)
-    
-    commentTimestamp.appendChild(commentName)
-    commentTimestamp.appendChild(commentDate)
-    commentName.appendChild(pDiv)
-    pDiv.appendChild(comment)
+for (i = 0; i < commentsArray.length; i++) {
+  displayComment(commentsArray[i]);
 }
 
+const submitBtn = document.getElementById("submit");
+const formButton = document.getElementById("formId");
 
-for(i = 0; i < commentsArray.length; i++){
-    displayComment(commentsArray[i])
-}
+formButton.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const newUserName = event.target.name.value;
+  const newUserComment = event.target.comment.value;
+  const currentDate = new Date()
 
-/*
-1. add event listener to "submit"
-const submit = document.getElementById("submit")
+  const newText = {
+    user: newUserName,
+    date: currentDate.toLocaleDateString("en-US"),
+    comment: newUserComment,
+  };
 
-submit.addEventListener('submit', (event)=>{
-  event.preventDefault()
-
-   const newUserName = event.target.name.value
-   const newUserComment = event.target.comment.value
-
-
-  const commentContainer = document.createElement("div")
-  commentContainer.classList.add("comment-container")
-
-  commentContainer.prepend(event)
-})
-
-2. store the date (new comments)
-x.toLocaleDateString("en-US")
-3. append new comments with time stamp to the mommy container
-*/
-
-// const submitBtn = document.getElementById("submit")
-// console.log(submitBtn)
-
-// console.log('\n')
-
-// submit.addEventListener('submit', (event)=>{
-  
-//   event.preventDefault()
-
-//   //storing the data in variables
-//    const newUserName = event.target.name.value
-//    const newUserComment = event.target.comment.value
-//    const newComment = displayComment({name:newUserName, date:date.toLocaleDateString("en-US"), comment:newUserComment})
-
-// //whenever the submit button is clicked, we want it to prepend to the parent container
-
-
-
-//   comments.prepend(newComment)
-  
-
-
-
-// })
-
+  displayComment(newText);
+});
